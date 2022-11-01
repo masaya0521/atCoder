406c8f07145a8f81e4536c61f75f4a7095131ed2
masaya0521 <kaji0521km@gmail.com>
2022-11-01 18:08:12 +0900

    attension

--- C/Attention.cpp
+++ C/Attention.cpp
@@ -2,24 +2,25 @@
 #include <vector>
 
 using namespace std;
-int n;
-char s[300005];
-int sum1[300005],sum2[300005],ans=1e9;
-int main()
-{
-	cin>>n>>s+1;
-	for(int i=1;i<=n;i++)
-	{
+
+int main(){
+    int n;
+    char s[300001];
+    int sum1[300001],sum2[300001],ans=0;
+
+    cin >> n >> s+1;
+
+
+	for(int i=1;i<=n;i++){
 		sum1[i]=sum1[i-1]+(s[i]=='W');
 		//cout<<sum1[i]<<" ";
 	}
-	for(int i=n;i>=1;i--)
-	{
+	for(int i=n;i>=1;i--){
 		sum2[i]=sum2[i+1]+(s[i]=='E');
 	}
-	for(int i=1;i<=n;i++)
-	{
+	for(int i=1;i<=n;i++){
 		ans=min(sum1[i-1]+sum2[i+1],ans);
 	}
-	cout<<ans;
+
+	cout << ans;
 }
