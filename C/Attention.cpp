2a87153807ff927b8af8f42b9603fee0e6bdcb68
masaya0521 <kaji0521km@gmail.com>
2022-11-01 18:01:48 +0900

    test

--- C/Attention.cpp
+++ C/Attention.cpp
@@ -2,14 +2,24 @@
 #include <vector>
 
 using namespace std;
-
-int main(){
-    int N;
-    cin >> N;
-    vector<int > A(N, 0);
-    vector<int > s(N+1, 0);
-    // 累積和
-    for(int i=0;i<N;i++){
-        s[i+1] = s[i] + A[i];
-    }
+int n;
+char s[300005];
+int sum1[300005],sum2[300005],ans=1e9;
+int main()
+{
+	cin>>n>>s+1;
+	for(int i=1;i<=n;i++)
+	{
+		sum1[i]=sum1[i-1]+(s[i]=='W');
+		//cout<<sum1[i]<<" ";
+	}
+	for(int i=n;i>=1;i--)
+	{
+		sum2[i]=sum2[i+1]+(s[i]=='E');
+	}
+	for(int i=1;i<=n;i++)
+	{
+		ans=min(sum1[i-1]+sum2[i+1],ans);
+	}
+	cout<<ans;
 }
