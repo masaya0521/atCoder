c7e7f44675855bd6b6df5c5fe49d33ef897ed50a
masaya0521 <kaji0521km@gmail.com>
2022-11-02 10:02:48 +0900

    add

--- D/Ksum.cpp
+++ D/Ksum.cpp
@@ -4,12 +4,21 @@
 using namespace std;
 
 int main(){
-    int N;
-    cin >> N;
-    vector<int > A(N, 0);
-    vector<int > s(N+1, 0);
+    long long N,K;
+    cin >> N >> K;
+
+    vector<long long > A(N, 0);
+    for(long long i=0;i<N;i++){
+        cin >> A[i];
+    }
+
+    vector<long long > s(N+1, 0);
     // 累積和
-    for(int i=0;i<N;i++){
+    for(long long i=0;i<N;i++){
         s[i+1] = s[i] + A[i];
+    }
+
+    for(long long i=0;i<=N-K;i++){
+        cout << s[i+K] - s[i] << endl;
     }
 }
