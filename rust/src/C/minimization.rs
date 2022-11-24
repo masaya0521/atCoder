d39e61955c934bab48d1563b19a5a2e6574d2c83
masaya0521 <kaji0521km@gmail.com>
2022-11-24 23:44:17 +0900

    minimazation

--- rust/src/C/minimization.rs
+++ rust/src/C/minimization.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("hello")
+    proconio::input! {
+        mut n: i128,
+        mut k: i128,
+        mut a: [i128;n],
+    }
+
+    // 1回目をカウントする
+    let mut sum: i128 = 1;
+
+    // 2回目以降の操作は最小の数とk-1個の組が変換されるので
+    // (n-k)/(k-1)回操作が繰り返される
+    for _i in 0..(n-k)/(k-1) {
+        sum += 1;
+    }
+
+    // 上記のfor文だと余りの数がカウントされないので
+    // ここでカウント
+    if (n-k)%(k-1) > 0  {
+        sum += 1;
+    }
+
+    println!("{}", sum);
 }
